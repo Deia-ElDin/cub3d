cd4e06598dc1877e848419b2b35c4e6de3ad9740
Deia Hamad <deia.tech2021@gmail.com>
2024-08-31 12:50:12 +0400

    updated exit clean

--- src/utils/exit.c
+++ src/utils/exit.c
@@ -3,40 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aalshafy <aalshafy@student.42abudhabi.a    +#+  +:+       +#+        */
+/*   By: dehamad <dehamad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 16:38:30 by dehamad           #+#    #+#             */
-/*   Updated: 2024/08/31 11:57:57 by aalshafy         ###   ########.fr       */
+/*   Updated: 2024/08/31 12:49:49 by dehamad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	exit_clean(t_cub *cub)
+static void	exit_mlx(t_cub *cub)
 {
-	ft_free(&cub->file.file_arr, 'a');
-	ft_free(&cub->map->map_arr, 'a');
 	if (cub->txtrs)
 	{
-		// mlx_distory_image(cub->mlx_ptr, cub->txtrs->no->img);
-		// mlx_distory_image(cub->mlx_ptr, cub->txtrs->so->img);
-		// mlx_distory_image(cub->mlx_ptr, cub->txtrs->we->img);
-		// mlx_distory_image(cub->mlx_ptr, cub->txtrs->ea->img);
+		if (cub->txtrs->no->img)
+			mlx_destroy_image(cub->mlx_ptr, cub->txtrs->no->img);
+		if (cub->txtrs->so->img)
+			mlx_destroy_image(cub->mlx_ptr, cub->txtrs->so->img);
+		if (cub->txtrs->we->img)
+			mlx_destroy_image(cub->mlx_ptr, cub->txtrs->we->img);
+		if (cub->txtrs->ea->img)
+			mlx_destroy_image(cub->mlx_ptr, cub->txtrs->ea->img);
 		ft_free(&cub->txtrs->no, 'p');
 		ft_free(&cub->txtrs->so, 'p');
 		ft_free(&cub->txtrs->we, 'p');
 		ft_free(&cub->txtrs->ea, 'p');
 		ft_free(&cub->txtrs, 'p');
 	}
+	if (cub->img->img)
+		mlx_destroy_image(cub->mlx_ptr, cub->img->img);
+	ft_free(&cub->img, 'p');
+	if (cub->win_ptr)
+	{
+		mlx_destroy_window(cub->mlx_ptr, cub->win_ptr);
+		free(cub->mlx_ptr);
+	}
+}
+
+static void	exit_clean(t_cub *cub)
+{
+	ft_free(&cub->file.file_arr, 'a');
+	ft_free(&cub->map->map_arr, 'a');
 	ft_free(&cub->map, 'p');
 	ft_free(&cub->player, 'p');
-	// if (cub->img->img)
-	// 	mlx_destroy_image(cub->mlx_ptr, cub->img->img);
-	ft_free(&cub->img, 'p');
 	ft_free(&cub->ray, 'p');
-	// if (cub->win_ptr)
-	// 	mlx_destroy_window(cub->mlx_ptr, cub->win_ptr);
-	// ft_free(&cub->mlx_ptr, 'p');
+	exit_mlx(cub);
 }
 
 void	exit_failure(t_cub *cub, char *err_msg)
